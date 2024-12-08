d3874158bfd67521cd4d4bd2f20b41fe74def3f6
Asif Ikbal Mondal <asikbal7ai@gmail.com>
2024-12-08 19:19:24 +0530

    added file

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const express = require("express")
+const { ApolloServer } = require("@apollo/server")
+const {expressMiddleware}=require("@apollo/server/express4")
+const { buildSchema } = require("graphql")
+const cors=require("cors")
+const bodYParser=require("body-parser")
+const bodyParser = require("body-parser")
+const { default: axios } = require("axios")
+
+
+async function startServer(){
+    const app=express();
+    const server=new ApolloServer({
+        typeDefs:`
+        type User {
+            id:ID!
+            name:String!
+            username:String!
+            email:String!
+            phone:String!
+            website:String!
+        }
+            type Todo {
+                id:ID!
+                title:String!
+                completed:Boolean
+            }
+            type Query{
+                getTodo:[Todo]
+                getAllUser:[User]
+                getUser(id: ID!):User
+            }
+        `,
+        resolvers: {
+            Query: {
+                getTodo: async () => (await axios.get(`https://jsonplaceholder.typicode.com/todos`)).data,
+                getAllUser: async () => (await axios.get(`https://jsonplaceholder.typicode.com/users`)).data,
+                getUser: async (parent,{id}) => (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)).data,
+            },
+        },
+    });
+
+    await server.start();
+    app.use(bodyParser.json());
+    app.use(cors());
+    app.use("/graphql",expressMiddleware(server));
+    app.listen(8000,()=>console.log("server started 8000"))
+}
+
+
+startServer();
